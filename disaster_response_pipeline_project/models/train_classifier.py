9f0c9115cde3472d737dcbc88204358b4d4abe71
racheldrysdale <46193240+racheldrysdale@users.noreply.github.com>
2021-10-27 17:35:57 +0100

    Add files via upload

---
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -0,0 +1,130 @@
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from nltk import word_tokenize
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+import pickle
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+def load_data(database_filepath):
+
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('disaster_response', engine)
+
+    # Features
+    X = df['message']
+
+    # Targets
+    Y = df.drop(columns=['id', 'message', 'original', 'genre'])
+
+    # Category names
+    category_names = Y.columns
+
+    return X, Y, category_names
+
+
+def tokenize(text):
+
+    # normalize
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # tokenize text
+    tokens = word_tokenize(text)
+
+    # lemmatize and remove stopwords
+    tokens = [WordNetLemmatizer().lemmatize(word) for word in tokens if word not in stopwords.words("english")]
+
+    return tokens
+
+
+def build_model():
+
+    # Create model pipeline
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ]))
+        ])),
+        ('clf', MultiOutputClassifier(DecisionTreeClassifier()))
+    ])
+
+    # Providing parameters for GridSearch
+    parameters = {
+        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'clf__estimator__max_depth': [10, 20],
+        'clf__estimator__min_samples_split': [2, 5],
+        'clf__estimator__ccp_alpha': [0.0, 0.1]
+    }
+
+    # Build model using GridSearch
+    model = GridSearchCV(pipeline, param_grid=parameters, cv=2, verbose=3)
+
+    return model
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+
+    # Create predictions
+    Y_pred = model.predict(X_test)
+
+    # Turn predictions into a dataframe
+    Y_pred_df = pd.DataFrame(Y_pred, index=Y_test.index, columns=category_names)
+
+    # Print classification report
+    print(classification_report(Y_test, Y_pred_df, target_names=category_names, zero_division=0))
+
+
+def save_model(model, model_filepath):
+
+    # Save model as a pickle file
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
