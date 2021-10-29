51513696849456f20f4bdb0526552b87fc19954c
racheldrysdale <46193240+racheldrysdale@users.noreply.github.com>
2021-10-29 16:04:23 +0100

    Update process_data.py
    Added docstrings to functions

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -4,6 +4,16 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    '''
+    Takes the messages and categories datasets and returns a merged dataframe.
+    
+    INPUT:
+    messages_filepath - (str) filepath of the messages CSV file
+    messages_filepath - (str) filepath of the categories CSV file
+    
+    OUTPUT:
+    df - (pandas dataframe) merged dataframe of the two datasets, joined on "id"
+    '''
 
     # Load the datasets
     messages = pd.read_csv(messages_filepath)
@@ -16,6 +26,15 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+    '''
+    Cleans and reformats the input dataframe including splitting out the message categories and dropping duplicates.
+    
+    INPUT:
+    df - (pandas dataframe) input dataframe to be cleaned and reformatted
+    
+    OUTPUT:
+    df - (pandas dataframe) cleaned dataframe with duplicates dropped, column added for each category, and unusual values dealt with
+    '''
 
     # Create a dataframe of the 36 individual category columns
     categories = df["categories"].str.split(';', expand=True)
@@ -52,12 +71,23 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    '''
+    Saves clean dataframe to an SQLite database.
+    
+    INPUT:
+    df - (pandas dataframe) dataframe to be saved
+    database_filename - (str) name the database should be saved under
+    '''
 
     engine = create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('disaster_response', engine, if_exists="replace", index=False)
 
 
 def main():
+    '''
+    Script to execute importing of data, cleaning of dataframe, and saving of SQLite database.
+    '''
+    
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
